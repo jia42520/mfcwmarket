04425cb7c3692652d2cc10933f3cefb970d24b68
jia42520 <jia42520@gmail.com>
2026-05-22 22:23:16 +0800

    Upload menu.php

---
+++ menu.php
@@ -0,0 +1,44 @@
+<?php
+
+return [
+    [
+        'name' => '交易市场',
+        'url' => 'ServerMarket://AdminIndex/index',
+        'custom' => 0,
+        'lang' => [
+            'chinese' => '交易市场',
+            'chinese_tw' => '交易市場',
+            'english' => 'Market',
+        ],
+    ],
+    [
+        'name' => '成交记录',
+        'url' => 'ServerMarket://AdminIndex/trades',
+        'custom' => 0,
+        'lang' => [
+            'chinese' => '成交记录',
+            'chinese_tw' => '成交記錄',
+            'english' => 'Trades',
+        ],
+    ],
+    [
+        'name' => '市场设置',
+        'url' => 'ServerMarket://AdminIndex/settings',
+        'custom' => 0,
+        'lang' => [
+            'chinese' => '市场设置',
+            'chinese_tw' => '市場設定',
+            'english' => 'Settings',
+        ],
+    ],
+    [
+        'name' => '审计日志',
+        'url' => 'ServerMarket://AdminIndex/logs',
+        'custom' => 0,
+        'lang' => [
+            'chinese' => '审计日志',
+            'chinese_tw' => '審計日誌',
+            'english' => 'Logs',
+        ],
+    ],
+];
